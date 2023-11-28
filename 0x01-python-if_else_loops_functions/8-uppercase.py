a48c050ed4923d8508b3ed9bb7e63e73df5bd459
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-11-28 07:53:48 -0800

    best

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+def uppercase(s):
+    for char in s:
+        # Check if the character is lowercase using ASCII values
+        if ord('a') <= ord(char) <= ord('z'):
+            # Convert the lowercase character to uppercase using ASCII values
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            print(uppercase_char, end='')
+        else:
+            # Print non-lowercase characters as they are
+            print(char, end='')
+
+    print()
