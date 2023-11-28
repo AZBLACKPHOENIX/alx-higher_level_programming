46874b18e81bd289ad099beca7d64c7e99c8b0ac
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-11-28 06:43:41 -0800

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ x = str(number)
 y = x[-1]
 z = int(y)
 if number < 0:
- z = -(z)
+    z = -(z)
 if number < 0:
     print("Last digit of %d is %s and is less than 6 and not 0" % (number, z))
 elif number > 0 and z < 5:
