9b7aa16c2607d7dd778084035c2a308b994b8ce0
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-11-30 09:36:00 -0800

    add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
+# Importing the add function from add_0.py
 from add_0 import add
+
+# Assigning values to variables
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+
+# Performing addition and displaying the result using print with string format
+print("{} + {} = {}".format(a, b, add(a, b)))
