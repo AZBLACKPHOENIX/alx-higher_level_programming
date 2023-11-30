03776b9f3a464a6d06e8950d1ea1ac5b244488ae
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-11-30 11:12:38 -0800

    ?

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+def magic_calculation(a, b):
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+
+# Test the function
+result = magic_calculation(10, 5)
+print(result)
+
