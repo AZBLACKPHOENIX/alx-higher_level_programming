552ff36382c59c2b53c4958cf6cbbb743a1fd87e
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-01 11:27:50 -0800

    a

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-def print_hidden_4_names():
-    names = dir(hidden_4)
-    for name in sorted(names):
-        if not name.startswith("__"):
-            print(name)
+from sys import argv
 
 if __name__ == "__main__":
-    print_hidden_4_names()
+    def calculate_args_sum():
+        args_sum = sum(int(arg) for arg in argv[1:])
+        print(args_sum)
 
+    calculate_args_sum()
