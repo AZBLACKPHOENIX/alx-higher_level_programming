8ce71aae8742525da2b15653187cb461435b95c8
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-01 11:01:21 -0800

    args

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
 
-names = dir(hidden_4)
-for name in sorted(names):
-    if not name.startswith("__"):
-        print(name)
+def print_hidden_4_names():
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
+
+if __name__ == "__main__":
+    print_hidden_4_names()
 
