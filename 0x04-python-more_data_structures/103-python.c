0bd05ae35a5a2234d22a9d527403161e263e2020
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-06 09:33:18 -0800

    cpython

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,61 @@
+#include <Python.h>
+
+/**
+ * print_python_list - Prints some basic info about Python lists
+ * @p: PyObject list
+ */
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t size, alloc, i;
+    PyObject *item;
+
+    size = PyList_Size(p);
+    alloc = ((PyListObject *)p)->allocated;
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", alloc);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+
+        if (PyBytes_Check(item))
+            print_python_bytes(item);
+    }
+}
+
+/**
+ * print_python_bytes - Prints some basic info about Python bytes objects
+ * @p: PyObject bytes
+ */
+void print_python_bytes(PyObject *p)
+{
+    Py_ssize_t size, i;
+    char *str;
+
+    printf("[.] bytes object info\n");
+
+    if (!PyBytes_Check(p))
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    str = PyBytes_AsString(p);
+
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", str);
+
+    printf("  first 10 bytes: ");
+    for (i = 0; i < size && i < 10; i++)
+    {
+        printf("%02x", (unsigned char)str[i]);
+        if (i < 9)
+            printf(" ");
+    }
+    printf("\n");
+}
+
