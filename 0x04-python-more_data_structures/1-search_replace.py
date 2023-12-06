e5a2370ae5bfa9387de8ddfb78b70e437598b3e6
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-06 07:59:20 -0800

    search_replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+def search_replace(my_list, search, replace):
+    # Create a new list to store the replaced values
+    new_list = []
+
+    # Iterate through each element in the original list
+    for element in my_list:
+        # Replace the element if it matches the search value, otherwise keep the original element
+        new_list.append(replace if element == search else element)
+
+    return new_list
