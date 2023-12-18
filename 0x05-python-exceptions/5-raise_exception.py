2a6cf4beab98588ca8106bb24811ca1682990ef5
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-18 11:39:50 -0800

    exc

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -3,9 +3,3 @@ def raise_exception():
     # Raise a type exception
     raise TypeError("Type exception")
 
-# Test case
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
-
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,4 @@
+def raise_exception_msg(message=""):
+    # Raise a name exception with the provided message
+    raise NameError(message)
+
