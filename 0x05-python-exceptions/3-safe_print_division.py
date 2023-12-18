83d62424fd5de0dfba07c7ddbd5aad825cba393a
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-18 11:13:17 -0800

    division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def safe_print_division(a, b):
     result = None
 
@@ -7,12 +6,12 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         # Handle division by zero
-        print("Inside result: None")
-    except (ValueError, TypeError):
-        # Handle unexpected value or type errors
+        result = None
         print("Inside result: None")
     finally:
-        # Print the result with "Inside result:" regardless of success or failure
-        print("Inside result: {:d}".format(result) if isinstance(result, int) else result)
+        # Print the result with "Inside result:" if it's not None
+        if result is not None:
+            print("Inside result: {:d}".format(result) if isinstance(result, int) else result)
 
     return result
+
