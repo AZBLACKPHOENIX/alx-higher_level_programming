80559a8a0f3a3efd2d1daaddff26dfe3ae57b1aa
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-19 11:15:51 -0800

    -----

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+class Node:
+    """Define a node of a singly linked list."""
+    def __init__(self, data, next_node=None):
+        """Initialize a node with data and next_node."""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Retrieve the value of data."""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Set the value of data with validation."""
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieve the value of next_node."""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Set the value of next_node with validation."""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Define a singly linked list."""
+    def __init__(self):
+        """Initialize an empty singly linked list."""
+        self.head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node into the correct sorted position in the list."""
+        new_node = Node(value)
+        if self.head is None or self.head.data >= value:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """Print the entire list in stdout."""
+        result = ""
+        current = self.head
+        while current is not None:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result
+
+if __name__ == "__main__":
+    SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
+
+    sll = SinglyLinkedList()
+    sll.sorted_insert(2)
+    sll.sorted_insert(5)
+    sll.sorted_insert(3)
+    sll.sorted_insert(10)
+    sll.sorted_insert(1)
+    sll.sorted_insert(-4)
+    sll.sorted_insert(-3)
+    sll.sorted_insert(4)
+    sll.sorted_insert(5)
+    sll.sorted_insert(12)
+    sll.sorted_insert(3)
+    print(sll)
+
