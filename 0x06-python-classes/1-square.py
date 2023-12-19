2d881f388c8fa7abadec101334c2d2c54b89e706
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-12-19 10:25:58 -0800

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define a square"""
+
+class Square:
+    """instintiate square"""
+    def __init__(self, size = 0):
+        """initialize square size"""
+        self.__size = size
