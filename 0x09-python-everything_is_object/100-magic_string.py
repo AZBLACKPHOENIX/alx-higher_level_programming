192237c97c22a81e7203ada8e4d41d1b77dbe95a
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2024-01-05 11:05:57 -0800

    EVERYTHING_IS_OBJECT

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-magic_string = lambda x=[0]: 'BestSchool, ' * x[0] + 'BestSchool' if x.__setitem__(0, x[0] + 1) is None else ''
-
+magic_string = lambda x=[0]: 'BestSchool, ' * x[0] + 'BestSchool$' if x.__setitem__(0, x[0] + 1) is None else ''
