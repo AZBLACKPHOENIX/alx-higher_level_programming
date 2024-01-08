ef0521f8154d3d44ab477203ceb0522cc7c1acb2
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2024-01-08 13:07:44 -0800

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define class"""
+class BaseGeometry:
+    """A class representing the base geometry."""
+
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
