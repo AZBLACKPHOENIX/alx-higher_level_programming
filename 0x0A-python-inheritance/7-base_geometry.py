19096edf58317350987dc548889d9e8d03b72255
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2024-01-08 13:12:23 -0800

    inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""define class"""
+class BaseGeometry:
+    """A class representing the base geometry."""
+
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
