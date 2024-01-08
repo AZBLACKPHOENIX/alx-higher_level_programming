04c0611fab17ab839c3c9e86e845bafe60eb25aa
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2024-01-08 13:16:53 -0800

    inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,14 +2,14 @@
 """define class"""
 class Rectangle(BaseGeometry):
     """A class representing a rectangle, inheriting from BaseGeometry."""
-
+    """define method"""
     def __init__(self, width, height):
         """Initialize the Rectangle instance."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
+    """define method"""
     def __str__(self):
         """Return a string representation of the Rectangle."""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
