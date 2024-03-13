15d09653bba0c21e378d2363e46a0325b7f1eb06
AZBLACKPHOENIX <125438999+AZBLACKPHOENIX@users.noreply.github.com>
2024-03-13 07:17:47 +0200

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++
@@ -1,9 +0,0 @@
--- Select tv_shows.title and tv_show_genres.genre_id from tv_shows and tv_show_genres tables
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
--- Join tv_shows and tv_show_genres tables on tv_shows.id equals tv_show_genres.show_id
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
--- Ensure that there is at least one genre linked for each show
-WHERE tv_shows.id IN (SELECT DISTINCT show_id FROM tv_show_genres)
--- Sort results in ascending order by tv_shows.title and tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- 0x0E-SQL_more_queries/3-force_name.sql
+++
@@ -1,7 +0,0 @@
---Create table force_name if it doesnt exist
-USE hbtn_0d_2;
-
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-);
